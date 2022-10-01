bd199249badf8f92dbd348478748a0f4fda4c7f7
HP <someshghuge85@gmail.com>
2022-10-02 00:13:00 +0530

    70% Project Complete

---
+++ ProjectQuiz/src/Quiz/Quetion.java
@@ -0,0 +1,14 @@
+package Quiz;
+
+import java.util.Random;
+import java.util.Random;
+public class Quetion {
+	String Que;
+	String Ans;
+
+	public Quetion (String Que, String Ans){
+		this.Que=Que;
+		this.Ans=Ans;
+	}
+	
+}
