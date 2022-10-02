705da236b817b989c8b4024327c181fa32902d7e
SomeshGhuge <113898033+SomeshGhuge@users.noreply.github.com>
2022-10-02 21:21:32 +0530

    mini project with 70% complete

--- ProjectQuiz/src/Quiz/Quiz1.java
+++ ProjectQuiz/src/Quiz/Quiz1.java
@@ -193,3 +193,14 @@ public void TakeTest(Quetion []quetion) {
 		System.out.println("<----- Thank u------>");	
 }
 }
+
+import java.util.Random;
+public class Quetion {
+	String Que;
+	String Ans;
+
+	public Quetion (String Que, String Ans){
+		this.Que=Que;
+		this.Ans=Ans;
+	}
+	
